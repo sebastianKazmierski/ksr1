a591a1488b5d041032ced7906e4371d4cef4af36
rjehf <seba19983@o2.pl>
2020-03-15 14:15:32 +0100

    add feature Number Of Words Remove By StopL ist In Relation To Length Of Text After Stop List

---
+++ src/test/java/featuresModels/NumberOfWordsRemoveByStopListInRelationToLengthOfTextAfterStopListTest.java
@@ -0,0 +1,19 @@
+package featuresModels;
+
+import data.Article;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberOfWordsRemoveByStopListInRelationToLengthOfTextAfterStopListTest {
+
+    @Test
+    void extract() {
+        String content = "  Alice has a cat whose name is Bob. Alice like Bob.\nBob like Alice. They are on the Eiffel Tower. ";
+        Article article = new Article(content, List.of("USA"));
+        FeatureExtractor featureExtractor = new NumberOfWordsRemoveByStopListInRelationToLengthOfTextAfterStopList();
+        assertEquals(7.0 / 13, featureExtractor.extract(article), "0.001");
+    }
+}
