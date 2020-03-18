8c91472f20322929bb4ca8c5683edb96fbbdda9f
rjehf <seba19983@o2.pl>
2020-03-18 11:47:05 +0100

    add counter of all key words

---
+++ src/test/java/featuresModels/keyWords/NumberOfKeyWordsTest.java
@@ -0,0 +1,24 @@
+package featuresModels.keyWords;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class NumberOfKeyWordsTest {
+
+    @Mock
+    KeyWordHolder keyWordHolder;
+
+    @Test
+    void count() {
+        System.out.println();
+    }
+
+    @Test
+    void countAllKeyWords() {
+    }
+}
