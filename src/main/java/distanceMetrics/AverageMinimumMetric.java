5a6f79484fcf326e3e58e2b8e9e34b3b39713f76
rjehf <seba19983@o2.pl>
2020-03-19 10:16:51 +0100

    Add metrics from the lecture

---
+++ src/main/java/distanceMetrics/AverageMinimumMetric.java
@@ -0,0 +1,18 @@
+package distanceMetrics;
+
+import java.util.List;
+
+public class AverageMinimumMetric implements DistanceMeasurement {
+    @Override
+    public double count(List<Double> testElement, List<Double> trainElement) {
+        double min = 0.0;
+        double sum = 0.0;
+
+        for (int i = 0; i < testElement.size(); i++) {
+            min += Math.min(testElement.get(i), trainElement.get(i));
+            sum += testElement.get(i) + trainElement.get(i);
+        }
+
+        return min / (sum * 0.5);
+    }
+}
---
+++ src/main/java/distanceMetrics/MinMaxMetric.java
@@ -0,0 +1,23 @@
+package distanceMetrics;
+
+import java.util.List;
+
+public class MinMaxMetric implements DistanceMeasurement {
+    @Override
+    public double count(List<Double> testElement, List<Double> trainElement) {
+        double min = 0.0;
+        double max = 0.0;
+
+        for (int i = 0; i < testElement.size(); i++) {
+            if (testElement.get(i) > trainElement.get(i)) {
+                max += testElement.get(i);
+                min += trainElement.get(i);
+            } else {
+                max += trainElement.get(i);
+                min += testElement.get(i);
+            }
+        }
+
+        return min / max;
+    }
+}
